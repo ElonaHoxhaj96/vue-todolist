995c2e3285a44f3a44ddb1529a921744898c053f
ElonaHoxhaj96 <elona6915@gmail.com>
2024-06-26 23:26:15 +0200

    feat: Added lin-through

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const { createApp }= Vue;
 createApp({
     data() {
         return {
+            done: false,
             newItem: '',
             listItem: [
                 'Fare la spesa',
@@ -16,8 +17,11 @@ createApp({
         addItem() {
             this.listItem.unshift(this.newItem);
         },
+        remuveItem(index) {
+            this.done(listItem[index]) = !this.done(listItem[index]);
+        }
+        
     },
-    
 }).mount('#app')
 
 
