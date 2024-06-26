ae74cdf7230d53c7f3c175b5d20aa77da264bfe7
ElonaHoxhaj96 <elona6915@gmail.com>
2024-06-26 18:54:34 +0200

    feat: printed in page my list item and the click for adding a new item

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
-const { createApp } = Vue;
+const { createApp }= Vue;
 
 createApp({
-    data(){
-        return{
-            
+    data() {
+        return {
+            newItem: '',
+            listItem: [
+                'Fare la spesa',
+                'Fare il bucato',
+                'Passare dalla banca',
+                'Portare il cane dal veterinario'
+            ],
         }
-    }
-})
+    },
+    methods: {
+        addItem() {
+            this.listItem.unshift(this.newItem);
+        },
+    },
+    
+}).mount('#app')
+
+
